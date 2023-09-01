7c5294fdbeb8d4251b52caa440eb06c9dee4e71c
ElMistr <emmanuel.carrera@uao.edu.co>
2023-08-31 23:01:56 -0500

    Arreglos
    Se agregaron los arreglos para la entrega.

--- src/Modelo/Receptor.java
+++ src/Modelo/Receptor.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  @author esteban.vargas@uao.edu.co, Esteban Vargas Sanchez, Codigo 2221675
+ *  @author emmanuel.carrera@uao.edu.co, Emmanuel Carrera Cardona, Codigo 2221577
+ *  @author carlos_andres.garzon@uao.edu.co, Carlos Andres Garzon Guerrero, Codigo 2220968
+ *  @author joan.salcedo@uao.edu.co, Joan Sebastian Salcedo Obando, Codigo 2220769
+ *  @date 31 /Agosto/2023
+ *  @version 1.0
  */
 package Modelo;
 
